7f8204d5c33390b2395be28a5530de1c5380fb2a
ericssz <196277583+ericssz@users.noreply.github.com>
2025-03-07 17:09:57 -0500

    refactor: make `Rect` struct literal

--- crates/vide/src/api/rect.rs
+++ crates/vide/src/api/rect.rs
@@ -10,19 +10,21 @@ pub struct Rect {
   pub position: AnimatedProperty<(f32, f32)>,
   pub size: AnimatedProperty<(f32, f32)>,
   pub color: AnimatedProperty<Color>,
-  mesh: InstancedMesh,
-  instances: Vec<Instance>,
 }
 
-impl Rect {
-  pub fn new(
+impl Clip for Rect {
+  fn render(
+    &mut self,
     renderer: &mut Renderer,
-    position: AnimatedProperty<(f32, f32)>,
-    size: AnimatedProperty<(f32, f32)>,
-    color: AnimatedProperty<Color>,
-  ) -> Self {
+    pass: MutexGuard<wgpu::RenderPass<'_>>,
+    frame: u64,
+  ) {
+    let position = self.position.evaluate(frame);
+    let size = self.size.evaluate(frame);
+    let color = self.color.evaluate(frame);
+
     let shader = Shader::new(renderer, include_str!("rect.wgsl").into());
-    let mesh = InstancedMesh::new(
+    let mut mesh = InstancedMesh::new(
       renderer,
       vec![
         Vertex {
@@ -46,41 +48,20 @@ impl Rect {
       shader,
     );
 
-    Self {
-      position,
-      size,
-      color,
-      mesh,
-      instances: Vec::new(),
-    }
-  }
-}
-
-impl Clip for Rect {
-  fn render(
-    &mut self,
-    renderer: &mut Renderer,
-    pass: MutexGuard<wgpu::RenderPass<'_>>,
-    frame: u64,
-  ) {
-    let position = self.position.evaluate(frame);
-    let size = self.size.evaluate(frame);
-    let color = self.color.evaluate(frame);
-
-    self.instances.push(Instance {
+    let instance = Instance {
       matrix: (cgmath::Matrix4::from_translation(cgmath::Vector3::new(
         position.0, position.1, 0.0,
       )) * cgmath::Matrix4::from_nonuniform_scale(size.0, size.1, 1.0)
         * OPENGL_TO_WGPU_MATRIX)
         .into(),
       color: color.into(),
-    });
+    };
 
-    self.mesh.render(
+    mesh.render(
       pass,
       renderer.wgpu_device(),
       renderer.wgpu_queue(),
-      self.instances.drain(..).collect(),
+      vec![instance],
     );
   }
 }
